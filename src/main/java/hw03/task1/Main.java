211d1d64d9e6d6a79d94d10c266a0e9ba7beaefa
Daniil Smirnov <dansmi97@gmail.com>
2017-10-01 22:58:12 +0300

    Added hw03-related fixes

--- src/main/java/hw03/task1/Main.java
+++ src/main/java/hw03/task1/Main.java
@@ -7,8 +7,8 @@ public class Main {
     public static void main(String[] args) {
         Automaton.setMinimization(Automaton.MINIMIZE_HOPCROFT);
 
-        Automaton regexA = new RegExp("(a|b)*ab(a|b)*|(a|b)*a|b").toAutomaton(true);
-        Automaton regexB = new RegExp("(a|b)*(ab|ba)(a|b)*|a*|b").toAutomaton(true);
+        Automaton regexA = new RegExp("(a|b)*ab(a|b)*|(a|b)*a|b*").toAutomaton(true);
+        Automaton regexB = new RegExp("(a|b)*(ab|ba)(a|b)*|a*|b*").toAutomaton(true);
 
         System.out.println(regexA.toDot());
         System.out.println(regexB.toDot());
